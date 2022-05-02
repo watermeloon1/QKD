065cc4b5ddb0c931e3d3ba514db04818648378e9
WaterMeloon <ifkovics.barnabas@gmail.com>
2022-05-02 10:53:10 +0200

    Added the turbulence stregth function declaration default wind_speed.

--- include/tools.hpp
+++ include/tools.hpp
@@ -7,6 +7,6 @@ double zenit(double distance_to_earth, double distance_to_base);
 double to_wavenumber(double wavelenght);
 double beam_widening_vacuum(double link_distance, double telescope_aperture, double wavelenght);
 double beam_widening_atmosphere(double link_distance, double telescope_aperture, double wavelenght, double coherence_length);
-double turbulence_strength(double height_above_sea_level /*,double wind_speed*/);
+double turbulence_strength(double height_above_sea_level, double wind_speed = 21);
 
 #endif
